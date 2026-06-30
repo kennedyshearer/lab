8e677d22a84bc80caca925f4be6d28c2939e2c05
Kennedy Shearer <kshea009@gmail.com>
2026-06-30 19:32:25 -0400

    fix: remove S3 website configuration and update CloudFront outputs

--- ks-blog/terraform-js/main.tf
+++ ks-blog/terraform-js/main.tf
@@ -35,54 +35,54 @@ resource "aws_s3_bucket_ownership_controls" "website_ownership_controls" {
 resource "aws_s3_bucket_acl" "website_bucket_acl" {
 
   depends_on = [
-    aws_s3_bucket_ownership_controls.website,
-    aws_s3_bucket_pubnlic_access_block.website_bucket_public_access_block
+    aws_s3_bucket_ownership_controls.website_ownership_controls,
+    aws_s3_bucket_public_access_block.website_bucket_public_access_block
   ]
 
   bucket = aws_s3_bucket.website.id
   acl = "public-read"
 }
 
-# S3 Website Configuration
-resource "aws_s3_bucket_website_configuration" "website_configuration" {
-  bucket = aws_s3_bucket.website.id
-
-  index_document { 
-    suffix ="index.html"
-  }
-
-  error_document {
-    key = "error.html"
-  }
-}
-
 # S3 Bucket Policy
 resource "aws_s3_bucket_policy" "website_policy" {
   bucket = aws_s3_bucket.website.id
-  policy = jsonencode({
+
+  depends_on = [
+    aws_s3_bucket_ownership_controls.website_ownership_controls,
+    aws_s3_bucket_public_access_block.website_bucket_public_access_block
+  ]
+  
+  policy = jsonencode ({
     Version = "2012-10-17"
     Statement = [
       {
         Effect = "Allow"
         Principal = "*"
         Action = "s3:GetObject"
-        Resource = "$(aws_s3_bucket.website.arn)/*"
+        Resource = "${aws_s3_bucket.website.arn}/*"
       }
     ]
   })
 }
 
+# Origin Access Identity
+resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
+  comment = "OAI for Next.JS portfolio website"
+}
+
+# Local variable for origin ID
+locals {
+  origin_id = "S3-Website"
+}
+
 # CloudFront Distribution
 resource "aws_cloudfront_distribution" "website_distribution" {
   origin {
-    domain_name = aws_s3_bucket_website_configuration.website_configuration.website_endpoint
-    origin_id   = "S3-Website"
+    domain_name = aws_s3_bucket.website.bucket_regional_domain_name
+    origin_id   = local.origin_id
     
-    custom_origin_config {
-      http_port              = 80
-      https_port             = 443
-      origin_protocol_policy = "http-only"
-      origin_ssl_protocols   = ["TLSv1.2"]
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
     }
   }
   
@@ -90,9 +90,9 @@ resource "aws_cloudfront_distribution" "website_distribution" {
   default_root_object = "index.html"
   
   default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD"]
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
     cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "S3-Website"
+    target_origin_id = local.origin_id
     
     forwarded_values {
       query_string = false
@@ -107,6 +107,18 @@ resource "aws_cloudfront_distribution" "website_distribution" {
     max_ttl                = 86400
   }
   
+   custom_error_response {
+    error_code         = 403
+    response_code      = 200
+    response_page_path = "/index.html"
+  }
+
+  custom_error_response {
+    error_code         = 404
+    response_code      = 200
+    response_page_path = "/index.html"
+  }
+
   restrictions {
     geo_restriction {
       restriction_type = "none"
--- ks-blog/terraform-js/outputs.tf
+++ ks-blog/terraform-js/outputs.tf
@@ -1,7 +1,14 @@
-output "bucket_website_endpoint" {
-  value = aws_s3_bucket_website_configuration.website_configuration.website_endpoint
+output "cloudfront_distribution_id" {
+  value       = aws_cloudfront_distribution.website_distribution.id
+  description = "CloudFront distribution ID, useful for cache invalidations"
 }
 
 output "cloudfront_url" {
   value = aws_cloudfront_distribution.website_distribution.domain_name
+  description = "CloudFront distribution URL"
+}
+
+output "s3_bucket_regional_domain_name" {
+  value       = aws_s3_bucket.website.bucket_regional_domain_name
+  description = "S3 REST endpoint used as the CloudFront origin"
 }
