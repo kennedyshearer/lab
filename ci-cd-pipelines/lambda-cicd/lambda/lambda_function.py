1601b3e8f0e12d48e7a981b143e537ed08d83754
Kennedy Shearer <kshea009@gmail.com>
2026-07-08 00:35:40 -0400

    Fix requirements.txt path and update lambda response message

--- ci-cd-pipelines/lambda-cicd/lambda/lambda_function.py
+++ ci-cd-pipelines/lambda-cicd/lambda/lambda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from CICD, NEW TEST from VSCode!!!')
+        'body': json.dumps('Hello from CICD, SUPER NEW TEST from VSCode!!!')
     }
