4fcd666b42474e21a4949750ca4d7adcdd8eaf3e
Kennedy Shearer <kshea009@gmail.com>
2026-07-20 21:02:54 -0400

    feat: add AWS Lambda function example

---
+++ programming-fundamentals/javascript/lambda.js
@@ -0,0 +1,10 @@
+exports.handler = async (event) => {
+    console.log('Received event:', JSON.stringify(event, null, 2));
+
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+    
+    return response;
+}
