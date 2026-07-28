553f711dbf1f11e3d15c436fcec3b3ec7c0a4db6
Kennedy Shearer <kshea009@gmail.com>
2026-07-28 11:14:22 -0400

    feat: add Terraform if-else example

---
+++ programming-fundamentals/javascript/if_else.js
@@ -0,0 +1,10 @@
+// If / Else Statements
+
+let iacProvider = "terraform";
+
+if (iacProvider === "terraform") {
+    console.log("We are using Terraform");
+} else {
+    console.log("We are not using Terraform");
+}
+
